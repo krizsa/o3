3a31ca77998e9e3c256a98f1a9acf298fb83fcbe
o3builder <rik@javeline.nl>
2010-10-04 13:23:14 +0200

    node modules

--- modules/xml/test/test.js
+++ modules/xml/test/test.js
@@ -1,6 +1,6 @@
 var fs = require('fs');
 var sys = require('sys');
-var xml = require("../lib/node-xml-dom");
+var xml = require("../lib/node-o3-xml");
 
 function diff(node, node1) {
     if (node.nodeType != node1.nodeType)
